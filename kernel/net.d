eccf0ef59f6d1a93b5ef73fa619c69a525e4b34b
Ben Chen <ch3nj@mit.edu>
2019-11-29 23:01:43 -0500

    gitignore

--- kernel/net.d
+++
@@ -1,3 +0,0 @@
-kernel/net.o: kernel/net.c kernel/types.h kernel/param.h \
- kernel/memlayout.h kernel/riscv.h kernel/spinlock.h kernel/proc.h \
- kernel/net.h kernel/defs.h
--- user/nettests.d
+++
@@ -1,2 +0,0 @@
-user/nettests.o: user/nettests.c kernel/types.h kernel/net.h \
- kernel/stat.h user/user.h
